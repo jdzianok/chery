383519eeeaae3de79b04e0e35873e2341eb3f05c
Jakub <j.dzianok@gmail.com>
2020-02-12 14:29:45 +0100

    done simpleSteps section

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navigation from "../Navigation/NavigationComponent";
 import HeaderComponent from "./HeaderComponent";
 import ThreeColumns from "./ThreeColumns";
+import SimpleStepsComponent from "./SimpleStepsComponent";
 
 class HomeComponent extends Component {
   state = {};
@@ -11,6 +12,7 @@ class HomeComponent extends Component {
         <Navigation />
         <HeaderComponent />
         <ThreeColumns />
+        <SimpleStepsComponent />
       </div>
     );
   }
