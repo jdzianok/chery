b910671d67e5798880c062e43cf305f00aaa620c
Jakub <j.dzianok@gmail.com>
2020-03-03 12:16:44 +0100

    done register component

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import App from "./App";
 import "./scss/main.scss";
 import * as serviceWorker from "./serviceWorker";
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDygKU-UrhY9PeUr-4Qe8bS7t-TEV1GgHg",
+  authDomain: "chery-f46b8.firebaseapp.com",
+  databaseURL: "https://chery-f46b8.firebaseio.com",
+  projectId: "chery-f46b8",
+  storageBucket: "chery-f46b8.appspot.com",
+  messagingSenderId: "849428522580",
+  appId: "1:849428522580:web:52d973042156e5c3bbafc1",
+  measurementId: "G-WMHSGRYLJY"
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
