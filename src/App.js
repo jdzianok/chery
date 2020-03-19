95da7b67fc19dddc3d18f215c77b69fac45e1b10
Jakub <j.dzianok@gmail.com>
2020-03-19 12:57:06 +0100

    deploy last try

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/HomeComponent";
 import Register from "./components/Register/RegisterComponent";
