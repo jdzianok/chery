b2a6a681267f06d3fff3fdb912a9f08f8eb198fd
Jakub <j.dzianok@gmail.com>
2020-06-24 18:51:49 +0200

    does it work?

--- src/components/MultiStepForm/Step_1Component.js
+++ src/components/MultiStepForm/Step_1Component.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Step_1Component() {
-  return <div className="step-1">step 1</div>;
+function Step_1Component(props) {
+  return <div className="step-1"> big step 1</div>;
 }
 
 export default Step_1Component;
