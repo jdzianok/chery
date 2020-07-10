487861b49640f21d14af0840c01d8ca1ccc382aa
Jakub <j.dzianok@gmail.com>
2020-07-10 13:19:34 +0200

    add SummaryComponent and basic styling

--- src/components/MultiStepForm/SummaryComponent.js
+++ src/components/MultiStepForm/SummaryComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
+import items_logo from "../../assets/1_step.svg";
+import bags_logo from "../../assets/2_step.svg";
+import receivers_logo from "../../assets/3_step.svg";
+import address_logo from "../../assets/4_step.svg";
+import organizations_logo from "../../assets/organizations.svg";
 
 function SummaryComponent(props) {
   const { values } = props;
-  console.log(values.clothes);
+  // console.log(values.clothes);
   const changeDateFormat = inputDate => {
     // expects Y-m-d
     var splitDate = inputDate.split("-");
@@ -17,58 +22,119 @@ function SummaryComponent(props) {
     return day + "." + month + "." + year;
   };
 
+  let items = [];
+  let receivers = [];
+
+  const {
+    clothes,
+    toys,
+    books,
+    other,
+    kids,
+    mothers,
+    homeless,
+    handicapped,
+    elderly,
+    organization
+  } = values;
+  const pushItems = (clothes, toys, books, other) => {
+    if (clothes) {
+      items.push("ubrania");
+    }
+    if (toys) {
+      items.push("zabawki");
+    }
+    if (books) {
+      items.push("książki");
+    }
+    if (other) {
+      items.push("inne");
+    }
+    return items;
+  };
+  const pushReceivers = (kids, mothers, homeless, handicapped, elderly) => {
+    if (kids) {
+      receivers.push("dzieciom");
+    }
+    if (mothers) {
+      receivers.push("samotnym matkom");
+    }
+    if (homeless) {
+      receivers.push("bezdomnym");
+    }
+    if (handicapped) {
+      receivers.push("niepełnosprawnym");
+    }
+    if (elderly) {
+      receivers.push("osobom starszym");
+    }
+    return receivers;
+  };
+
+  pushItems(clothes, toys, books, other);
+  pushReceivers(kids, mothers, homeless, handicapped, elderly);
+  const itemsParagraf = items.map(item => (
+    <p key={item} className="wrap-up__info">
+      {item}
+    </p>
+  ));
+
+  const receiversParagraf =
+    receivers.length !== 0 ? (
+      receivers.map(item => (
+        <p key={item} className="wrap-up__info">
+          {item}
+        </p>
+      ))
+    ) : (
+      <p className="wrap-up__info">Brak preferencji</p>
+    );
+
   return (
-    <div className="summary">
-      <h2 className="summary__header formHeader">
+    <div className="wrap-up">
+      <h2 className="wrap-up__header formHeader">
         Podsumowanie Twojej darowizny
       </h2>
-      <div className="summary__youGive">
-        <h3 className="summary__youGive__header">Oddajesz:</h3>
-        <div className="summary__youGive__container">
-          <p className="summary__youGive__bags">
+      <div className="wrap-up__container">
+        <div className="wrap-up__box">
+          <img src={bags_logo} alt="" className="wrap-up__icon" />
+          <h3 className="wrap-up__title">Ile?</h3>
+          <p className="wrap-up__info">
             {values.bags}
-            {values.bags === "1" ? " worek" : " worki"}
-          </p>
-          {values.clothes ? (
-            <p className="summary__youGive__items">ubrania</p>
-          ) : null}
-          {values.toys ? (
-            <p className="summary__youGive__items">zabawki</p>
-          ) : null}
-          {values.books ? (
-            <p className="summary__youGive__items">książki</p>
-          ) : null}
-          {values.other ? (
-            <p className="summary__youGive__items">inne</p>
-          ) : null}
-        </div>
-        <div className="summary__youGive__container">
-          <p className="summary__youGive__location">
-            Lokalizacja: {values.location}
+            {values.bags === "1"
+              ? " worek"
+              : values.bags === "5"
+              ? " worków"
+              : " worki"}
           </p>
         </div>
-      </div>
-      <div className="summary__deliveryAddress">
-        <h3 className="summary__deliveryAddress__header">Adres odbioru:</h3>
-        <p className="summary__deliveryAddress__address">
-          Ulica: {values.street}
-        </p>
-        <p className="summary__deliveryAddress__address">
-          Miasto: {values.city}
-        </p>
-        <p className="summary__deliveryAddress__address">
-          Kod pocztowy: {values.postCode}
-        </p>
-        <p className="summary__deliveryAddress__address">
-          Telefon: {values.phone}
-        </p>
-      </div>
-      <div className="summary__pickUpTime">
-        <h3 className="summary__pickUpTime__header">Termin odbioru:</h3>
-        <p className="summary__pickUpTime__date">
-          Data: {changeDateFormat(values.date)}
-        </p>
-        <p className="summary__pickUpTime__date">Godzina: {values.hour}</p>
+        <div className="wrap-up__box">
+          <img src={items_logo} alt="" className="wrap-up__icon" />
+          <h3 className="wrap-up__title">Co?</h3>
+          {itemsParagraf}
+        </div>
+        <div className="wrap-up__box">
+          <img src={receivers_logo} alt="" className="wrap-up__icon" />
+          <h3 className="wrap-up__title">Komu?</h3>
+          {receiversParagraf}
+        </div>
+        {organization ? (
+          <div className="wrap-up__box">
+            <img src={organizations_logo} alt="" className="wrap-up__icon" />
+            <h3 className="wrap-up__title">Organizacja:</h3>
+            <p className="wrap-up__info">{organization}</p>
+          </div>
+        ) : null}
+        <div className="wrap-up__box">
+          <img src={address_logo} alt="" className="wrap-up__icon" />
+          <h3 className="wrap-up__title">Adres odbioru:</h3>
+          <p className="wrap-up__info">Ulica: {values.street}</p>
+          <p className="wrap-up__info">Miasto: {values.city}</p>
+          <p className="wrap-up__info">Kod pocztowy: {values.postCode}</p>
+          <p className="wrap-up__info">Telefon: {values.phone}</p>
+          <p className="wrap-up__info">Data: {changeDateFormat(values.date)}</p>
+          <p className="wrap-up__info">Godzina: {values.hour}</p>
+        </div>
       </div>
     </div>
   );
