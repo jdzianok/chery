f452d2dff22a32945c3ffe5c6ba110d165351bf1
Jakub <j.dzianok@gmail.com>
2020-10-01 14:02:10 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/chery/precache-manifest.353a54fbe3acce58aa56c3c3e6e5c717.js"
+  "/chery/precache-manifest.0f69db7b5d91581e0b7eaaf13628d50c.js"
 );
 
 self.addEventListener('message', (event) => {
